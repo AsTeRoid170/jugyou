5ad6a2413bafe9f533fa6e682dff32c979b15fcd
th <generalpenpengame@gmail.com>
2025-05-12 11:27:53 +0900

    player yatta

---
+++ Assets/Hashizume/script/PlayerController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 7f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float moveX = 0f;
+        if (Input.GetKey(KeyCode.A)) moveX = -1f;
+        if (Input.GetKey(KeyCode.D)) moveX = 1f;
+
+        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+
+        // Wジャンプ
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+
+        // Space攻撃
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void Attack()
+    {
+        Debug.Log("攻撃");
+        animator.SetTrigger("attack");  // ←アニメーション再生
+
+    }
+}
