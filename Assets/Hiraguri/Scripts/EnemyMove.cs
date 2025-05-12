de8a38b74cb5d89bcadb691648df08e25178c4ed
STUDENT\2311034 <0br6yp162k-042p@ezweb.ne.jp>
2025-05-12 12:26:33 +0900

    崖端で左右反転できた壁で反転はまだ

--- Assets/Hiraguri/Scripts/EnemyMove.cs
+++ Assets/Hiraguri/Scripts/EnemyMove.cs
@@ -7,8 +7,11 @@ public class EnemyMove : MonoBehaviour
     Rigidbody2D enemyRb;
     Collider2D enemyCol;
     [SerializeField] float moveSpeed = 5;
-    float distance = 0.1f;
-    int moveDirection = 1; 
+    Vector2 direction = Vector2.down;
+    float rayDistance = 0.3f;
+    float forwardDistance = -0.5f;
+    float footDistance = -0.7f;
+    bool enemyDirectionFlg = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,12 +23,34 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // à⁄ìÆ
-        transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        if (enemyDirectionFlg)
+        {
+            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
+        }
+        else
+        {
+            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
+        }
+        // Ç±Ç±ÇÃ-0.5Çç∂âEÇÃå¸Ç´Ç≈-ÇäOÇ∑(ïœêîÇópà”)
+        Vector2 origin = new Vector2(enemyRb.transform.position.x + forwardDistance, enemyRb.transform.position.y + footDistance);
+        RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, rayDistance);
+
+        Debug.DrawLine(origin, origin + direction * rayDistance, Color.red);
+
+        if(hit.collider == null)
+        {
+            Flip();
+        }
         
+    }
 
-        //RaycastHit2D hit = Physics2D.Raycast(origin, Vector2.down, 1);
-
+    private void Flip()
+    {
+        enemyDirectionFlg = !enemyDirectionFlg;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;  // Xé≤Ç≈îΩì]
+        transform.localScale = localScale;
+        forwardDistance *= -1;
     }
 
 }
