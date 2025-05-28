1aa8f6278cc54e0bc0344e78ba37b269bc44815f
STUDENT\2311034 <0br6yp162k-042p@ezweb.ne.jp>
2025-05-28 12:27:51 +0900

    弾を撃つ敵途中　詰まった

--- Assets/Enemy/Scripts/BulletEnemyControl.cs
+++ Assets/Enemy/Scripts/BulletEnemyControl.cs
@@ -1,22 +1,32 @@
 using UnityEngine;
 
-public class BulletEnemyAttack : MonoBehaviour
+public class BulletEnemyControl : MonoBehaviour
 {
-    [SerializeField ]GameObject bulletPoint;
+    [SerializeField] GameObject bulletPoint;
     [SerializeField] GameObject bullet;
     [SerializeField] float Interval = 5.0f;
-    
+    [SerializeField] GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         InvokeRepeating("Shoot", 0f, Interval);
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.x > transform.position.x)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+
+
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
     }
 
     private void Shoot()
