3232b5949c2334e1dd510b13d8627b035372f102
STUDENT\2311034 <0br6yp162k-042p@ezweb.ne.jp>
2025-06-11 11:03:02 +0900

    動き完成　あとはアニメーションとSE

--- Assets/Enemy/Scripts/BossEnemyController.cs
+++ Assets/Enemy/Scripts/BossEnemyController.cs
@@ -3,7 +3,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class BossEnemyControl : MonoBehaviour
+public class BossEnemyController : MonoBehaviour
 {
 
     // 状態を表すステータス
@@ -19,7 +19,6 @@ public class BossEnemyControl : MonoBehaviour
     GameObject player;                                  // プレイヤーの位置で行動を決めたい
     float direction;                                    // プレイヤーとの距離
     Vector2 playerPosition;                             // プレイヤーの位置
-    //Vector2 startPosition;
 
     // move用
     [SerializeField] float moveSpeed = 2f;                  // 歩くスピード
@@ -31,22 +30,20 @@ public class BossEnemyControl : MonoBehaviour
     // punch用
     [SerializeField] float punchDelay = 5.0f;               // 何秒間隔でパンチするか
     [SerializeField] float punchRange = 7f;                 // プレイヤーがどれくらいの距離でパンチするか
-    [SerializeField] GameObject punchHitBox;
+    [SerializeField] GameObject punchHitBox;                // パンチの当たり判定
+    float punchDelayTime;                                   // ディレイカウント用
 
     // beam用
-    [SerializeField] float beamRange = 15f;
-    [SerializeField] float beamDelay = 5.0f;
-    [SerializeField] GameObject beam;
-    [SerializeField] GameObject beamPoint;
-    float beamDelayTime;
-    float punchDelayTime;
-
-
+    [SerializeField] float beamRange = 15f;                 // どれくらいの距離でビームを撃つか
+    [SerializeField] float beamDelay = 5.0f;                // ビーム間隔
+    [SerializeField] GameObject beam;                       // ビームオブジェクト
+    [SerializeField] GameObject beamPoint;                  // ビーム発射位置
+    float beamDelayTime;                                    // ディレイカウント用              
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = GameObject.FindWithTag("Player");
-        //startPosition = transform.position;
         timer = changeDirectionInterval;
         punchDelayTime = punchDelay;
         beamDelayTime = beamDelay;
@@ -61,13 +58,13 @@ public class BossEnemyControl : MonoBehaviour
         {
             playerPosition = player.transform.position;
         }
+        // playerとの距離を計算
         direction = Mathf.Abs(playerPosition.x - transform.position.x);
-        Debug.Log(direction);
+        //Debug.Log(direction);
 
         // plyerのほうを向く
         flip();
 
-        
         // Idleへの切り替え
         if (player == null)
         {
@@ -76,7 +73,6 @@ public class BossEnemyControl : MonoBehaviour
         // Punchへの切り替え
         else if (direction < punchRange)
         {
-            //Debug.Log("a");
             ChangeState(State.Punch);
         }
         // Moveへの切り替え 
@@ -90,6 +86,7 @@ public class BossEnemyControl : MonoBehaviour
             ChangeState(State.Beam);
         }
 
+        // パンチ判定が出しっぱなしになることを避けるため
         if (currState != State.Punch)
         {
             punchHitBox.SetActive(false);
@@ -112,6 +109,7 @@ public class BossEnemyControl : MonoBehaviour
             case State.Punch:
                 
                 BossPunch();
+                // 攻撃判定が出ている最中は動かない
                 if(!punchHitBox.activeInHierarchy)
                 {
                     BossMove();
@@ -126,6 +124,7 @@ public class BossEnemyControl : MonoBehaviour
         }
     }
 
+    // Stateの変更
     private void ChangeState(State newState)
     {
         currState = newState;
@@ -133,9 +132,10 @@ public class BossEnemyControl : MonoBehaviour
 
     private void BossIdel()
     {
-
+        // 何もしない
     }
 
+    // 右往左往する
     private void BossMove()
     {
         if (player != null)
@@ -156,7 +156,6 @@ public class BossEnemyControl : MonoBehaviour
     {
         if(punchDelayTime > punchDelay)
         {
-            Debug.Log("a");
             punchHitBox.SetActive(true);
             
             punchDelayTime = 0;
@@ -167,10 +166,7 @@ public class BossEnemyControl : MonoBehaviour
             if (punchDelayTime > 0.5)
             {
                 punchHitBox.SetActive(false);
-            }
-            
-
-            
+            }  
         }
     }
 
@@ -189,9 +185,9 @@ public class BossEnemyControl : MonoBehaviour
         
     }
 
+    // プレイヤーの方を向く
     private void flip()
     {
-        // プレイヤーの方を向く
         if (player != null)
         {
             if (player.transform.position.x > transform.position.x)
